d8d21eb48987366a9061d536f5a977bafebb99f9
graceFor <tbgusrud12@gmail.com>
2020-07-22 17:35:34 +0900

    [wordcount]

--- Wordcount_project/wordcount/views.py
+++ Wordcount_project/wordcount/views.py
@@ -14,4 +14,12 @@ def about(request):
 def result(request):
     text = request.GET['fulltext']
     words = text.split()
-    return render(request, 'result.html', {'full': text})
+    words_dictionary = {}
+
+    for word in words:
+        if word in words_dictionary:
+            words_dictionary[word] += 1
+        else:
+            words_dictionary[word] = 1
+
+    return render(request, 'result.html', {'full': text, 'total': len(words), 'dic': words_dictionary.items()})
