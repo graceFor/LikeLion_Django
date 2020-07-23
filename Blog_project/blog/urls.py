b8b5bedb6a28f2fad570de16e3d5794a5ddc4e60
graceFor <tbgusrud12@gmail.com>
2020-07-23 12:58:56 +0900

    [blog]

--- Blog_project/blog/urls.py
+++ Blog_project/blog/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', blogapp.views.home, name='home'),
     path('news_blog/<int:blog_id>', blogapp.views.detail, name="detail"),
+    path('news_blog/new', blogapp.views.new, name='new'),
+    path('news_blog/create', blogapp.views.create, name='create'),
 ]
