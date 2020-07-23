2882cdeba945225b34732ede83c9281719a136ff
graceFor <tbgusrud12@gmail.com>
2020-07-23 23:41:25 +0900

    [blog]

--- Blog_project/account/views.py
+++ Blog_project/account/views.py
@@ -15,4 +15,21 @@ def signup(request):
 
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'username or password is incorrect.'})
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
     return render(request, 'login.html')
