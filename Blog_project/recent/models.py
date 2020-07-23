4db440e182ba3dc7bf1a98aac7fab70e477683a5
graceFor <tbgusrud12@gmail.com>
2020-07-23 16:00:17 +0900

    [image_pillow]

--- Blog_project/recent/models.py
+++ Blog_project/recent/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from blogapp.models import Blog
 # Create your models here.
+
+
+class Recent(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
